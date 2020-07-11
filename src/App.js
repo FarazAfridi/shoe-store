2970c384d2862603be1a1c36eefc86538792a917
FarazAfridi <farazafridi5159@gmail.com>
2020-07-11 20:23:50 +0500

    shoe-store-project-completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+import Shop from "./components/shop/shop"
+import Checkout from './components/checkout/checkout';
+import CartProvider from "./context/cart-context/cart-context";
 
 function App() {
   return (
+    <CartProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path='*' element={<h1>404 Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
+    </CartProvider>
   );
 }
 
---
+++ src/context/cart-context/cart-context.js
@@ -0,0 +1,44 @@
+import React, { useState, createContext, useEffect } from 'react';
+import { addCartItem, getItemCount, getTotal, filterItemFromCart,removeItemFromCart } from './utils';
+
+export const  CartContext = createContext({
+    hidden: true,
+    toggleHidden: () => {},
+    cartItems: [],
+    addItem: () => {},
+    removeItem: () => {},
+    clearItemFromCart: () => {},
+    cartItemCount: 0,
+    cartItemTotal: 0,
+});
+
+const CartProvider = ({ children }) => {
+    const [hidden, setHidden] = useState(true);
+    const [cartItems, setCartItems] = useState([]);
+    const [cartItemCount, setCartItemsCount] = useState(0);
+    const [cartItemTotal, setCartItemTotal] = useState(0);
+
+    useEffect(() => {
+        setCartItemsCount(getItemCount(cartItems));
+        setCartItemTotal(getTotal(cartItems));
+    }, [cartItems]);
+
+    const toggleHidden = () => setHidden(!hidden);
+    const addItem = (item) => setCartItems(addCartItem(cartItems, item));
+    const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
+    const clearItemFromCart = item => setCartItems(filterItemFromCart(cartItems, item));
+
+    return <CartContext.Provider value={{
+        hidden,
+        toggleHidden,
+        cartItems,
+        addItem,
+        cartItemCount,
+        cartItemTotal,
+        removeItem,
+        clearItemFromCart
+    }} >
+    {children}</CartContext.Provider>
+}
+    
+export default CartProvider;
